41bbdb999c5e4faab61409b413de8813b49b99d5
Aaron Rice <adrice727@gmail.com>
2014-05-19 22:57:04 -0700

    Still trying to figure out how to bind SVG elements to Angular controller

--- main/js/app.js
+++ main/js/app.js
@@ -1,35 +1,18 @@
 angular.module('travel_book', ['ui.router'])
 
 
+.controller('mainController', function($rootScope, $scope) {
 
+  $scope.selectCountry = function(){
+    alert('country clicked');  
+  }
+  // var test = angular.element('.country')
+  // console.log(test);
 
-.config(function($stateProvider, $urlRouterProvider) {
-    
-    $urlRouterProvider.otherwise('/');
-    
-    $stateProvider
-        
-        // HOME STATES AND NESTED VIEWS ========================================
-        .state('index', {
-
-            url: '/',
-             views: {
-              "map@": { templateUrl: "../views/map.html" },
-              "content@": { templateUrl: "" }
-            }
-        })
-        
-        // ABOUT PAGE AND MULTIPLE NAMED VIEWS =================================
-        // .state('about', {
-        //     // we'll get to this in a bit       
-        // });        
 })
 
-
-.controller('mainController', function($scope) {
-
-  $scope.things = 'hello plus 2';
-  console.log('getting here');
-
-
+.directive("countryClick", function(scope, element, attrs){
+  element.bind('click', function(){
+    scope.$apply("selectCountry()");
+  })
 })
